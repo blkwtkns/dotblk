b40c091960660b573ef2f2e032c882e78349778a
Blake Watkins <iamsoblake@gmail.com>
2017-03-03 00:21:09 -0800

    adjusting zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,5 +1,4 @@
-PROMPT='
-${PR_GREEN}%n%{$reset_color%} %{$FG[239]%}at%{$reset_color%} ${PR_BOLD_BLUE}$(box_name)%{$reset_color%} %{$FG[239]%}in%{$reset_color%} ${PR_BOLD_YELLOW}$(current_pwd)%{$reset_color%} $(git_prompt_string)
-$(prompt_char) '
+PROMPT='%{$fg[$prompt_color]%}%n@%m%{$reset_color%} %~
+%{${fg[yellow]}%}%# '
 
 export SPROMPT="Correct $fg[red]%R$reset_color to $fg[green]%r$reset_color [(y)es (n)o (a)bort (e)dit]? "
